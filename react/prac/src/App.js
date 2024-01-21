d52fd694ea621fc2cabf228a4d6496ffda6f1e20
hyeonjun4460 <hyeonjun4460@gmail.com>
2024-01-21 16:25:56 +0900

    특정 state 값 변경 트리거를 기준으로 useEffect 실행 구현(검색 구현)

--- react/prac/src/App.js
+++ react/prac/src/App.js
@@ -2,24 +2,41 @@ import { useEffect, useState } from 'react';
 import { Button } from './Button';
 import title from './Title.module.css';
 function App() {
+  const [state, setState] = useState(0);
+  const onClick = () => setState((prev) => prev + 1);
 
-  const [state, setState] = useState(0)
-  const onClick = () => setState((prev) => prev+1)
+  const [keyword, setKeyword] = useState('');
 
-  console.log('i am always called by changed state')
+  const onChange = (event) => {
+    console.log(`${keyword} -> ${event.target.value}`);
+    setKeyword(event.target.value);
+  };
+
+  // input value에 따른 검색 API 호출
+  useEffect(() => {
+    if (keyword.length > 3) console.log(`search for ${keyword}`);
+  }, [keyword]);
+
+  console.log('i am always called by changed state');
 
   const test = () => {
-    console.log('i am called one time when first rendering')
-  }
+    console.log('i am called one time when first rendering');
+  };
 
-  useEffect(test, [])
+  useEffect(test, []);
 
   return (
     <div className="App">
+      <input
+        onChange={onChange}
+        type="text"
+        value={keyword}
+        placeholder="press any thing"
+      ></input>
       <h1 className={title.title}>hi</h1>
       <p>{state}</p>
-      <Button onClick={onClick} text={'click me'}/>
-      <Button text={'hi'} />
+      <Button onClick={onClick} text={'click me'} />
+      {/* <Button text={'hi'} /> */}
     </div>
   );
 }
--- react/prac/src/Button.js
+++ react/prac/src/Button.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
-export function Button({ onClick,text }) {
-  return <button onClick={onClick} className={styles.btn}>{text}</button>;
+export function Button({ onClick, text }) {
+  return (
+    <button onClick={onClick} className={styles.btn}>
+      {text}
+    </button>
+  );
 }
 
 Button.propTypes = {
